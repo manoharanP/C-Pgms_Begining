bfbb7c4db841c97eb685a797f338b6d5bbb264d3
manoharanP <30316786+manoharanP@users.noreply.github.com>
2017-10-04 21:50:00 +0530

    Create delete_digit.java

---
+++ delete_digit.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class delete_digit {
+
+	public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+	int n=s.nextInt();
+	int[] a=new int[n];
+	int a;
+	for(a=0;a<n;a++){
+		a[a]=s.nextInt();
+	}
+	Arrays.sort(b);
+	System.out.println("Enter the k digit");
+	int k=s.nextInt();
+	for(a=0;a<n-k;a++){
+		System.out.println(b[a]);
+	}
+	}
+  }
